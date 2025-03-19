00262ad78dd123f721f1312cb3abc205be5e1220
Mauricio Torti <mauriciotorti1@gmail.com>
2025-03-19 19:02:03 -0300

    Registro de usuario y encriptacion de contraseña

--- user/api/urls.py
+++ user/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from user.api.views import RegisterView
 
 urlpatterns = [
-    path('auth/register/', RegisterView.as_view())
+    path('register/', RegisterView.as_view())
 ]
--- user/api/views.py
+++ user/api/views.py
@@ -2,8 +2,20 @@ from user.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from user.api.serializers import RegisterUserSerializer
 
 class RegisterView(APIView):
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = RegisterUserSerializer(users, many=True)
+        return Response(serializer.data)
+
     def post(self, request):
-        print('status registrado')
-        return Response( status=status.HTTP_201_CREATED)
+
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
